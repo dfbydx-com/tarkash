cba9b697267a35c6c68f1d2eb9dbe01b3d173235
Dilshad-create <mddilshadansari.mat18@itbhu.ac.in>
2022-01-08 15:19:18 +0530

    Search in ytapp

--- ytapp/main.go
+++ ytapp/main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/Dilshad-create/ytapp/youtube"
 )
 
-// func homePage(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprint(w, "I won't print hello world this time")
-// }
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I don't want to write 'hello world' this time")
+}
 
-// func setupRoutes() {
-// 	http.HandleFunc("/", homePage)
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func main() {
 	fmt.Println("Youtube API tests")
-	item, err := youtube.GetSubscribers("UClQf58OQvm6OkGbQARqqhMg")
+	// item, err := youtube.GetSubscribers("UClQf58OQvm6OkGbQARqqhMg") //attributes:channel id
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// //fmt.Printf("%+v\n", item)
+	// youtube.PrintGetSubscribers(item)
+
+	resp, err := youtube.Search("new year", "5") //attributes: query,maxResults
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%+v\n", item)
+	//fmt.Printf("%+v\n", resp)
+	youtube.PrintSearch(resp)
 
 	//setupRoutes()
 }
--- ytapp/youtube/youtube.go
+++ ytapp/youtube/youtube.go
@@ -74,3 +74,112 @@ func GetSubscribers(id string) (Item, error) {
 
 	return response.Items[0], nil
 }
+
+type SearchResp struct {
+	Kind          string       `json:"kind"`
+	NextPageToken string       `json:"nextPageToken"`
+	PageInfo      PageInfo     `json:"pageInfo"`
+	Items         []SearchItem `json:"items"`
+}
+type PageInfo struct {
+	TotalResults   int64 `json:"totalResults"`
+	ResultsPerPage int64 `json:"resultsPerPage"`
+}
+type SearchItem struct {
+	Kind    string        `json:"kind"`
+	Id      SearchId      `json:"id"`
+	Snippet SearchSnippet `json:"snippet"`
+}
+type SearchId struct {
+	Kind    string `json:"kind"`
+	VideoId string `json:"videoId"`
+}
+type SearchSnippet struct {
+	PulishedAt   string     `json:"publishedAt"`
+	ChannelId    string     `json:"channelId"`
+	Title        string     `json:"title"`
+	Desc         string     `json:"description"`
+	Thumbnails   Thumbnails `json:"thumbnails"`
+	ChannelTitle string     `json:"channelTitle"`
+}
+type Thumbnails struct {
+	Default Thumbnail `json:"default"`
+	Medium  Thumbnail `json:"medium"`
+	High    Thumbnail `json:"high"`
+}
+type Thumbnail struct {
+	Url    string `json:"url"`
+	Width  int64  `json:"width"`
+	Height int64  `json:"height"`
+}
+
+func Search(query string, maxResults string) (SearchResp, error) {
+	response := SearchResp{}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://youtube.googleapis.com/youtube/v3/search", nil)
+	if err != nil {
+		fmt.Println(err)
+		return response, err
+	}
+
+	creds := YoutubeCreds{}
+	err = gonfig.GetConf("./youtube/config.json", &creds)
+	if err != nil {
+		fmt.Println(err)
+		return response, err
+	}
+
+	q := req.URL.Query()
+	q.Add("part", "snippet")
+	q.Add("q", query) // new year should be new%20year
+	q.Add("key", creds.Key)
+	q.Add("maxResults", maxResults)
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return response, err
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status: ", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return response, err
+	}
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+		return response, err
+	}
+
+	return response, nil
+}
+
+func PrintSearch(response SearchResp) {
+	bytes, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("./print.json", bytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Result is saved in './print.json' ")
+}
+func PrintGetSubscribers(item Item) {
+	bytes, err := json.Marshal(item)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("./print.json", bytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Result is saved in './print.json' ")
+}
